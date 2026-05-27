aebc5ae8e266cb32825420bccfdc429c8c84636c
Patricia Lacerda <patriciagolfete@gmail.com>
2026-05-27 10:57:12 -0300

    Projeto inicial

---
+++ Documents/NetBeansProjects/BiblioTec/src/main/java/com/mycompany/bibliotec/BiblioTec.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bibliotec;
+
+/**
+ *
+ * @author Patricia
+ */
+public class BiblioTec {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
