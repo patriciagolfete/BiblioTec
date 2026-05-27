f14ceac7bc6c9a533b9322127b2b28afafe097eb
Patricia Lacerda <patriciagolfete@gmail.com>
2026-05-27 13:37:35 -0300

    CRUD completo de livros

--- Downloads/bibliotec-api/bibliotec-api/src/main/java/com/bibliotec/bibliotec_api/controller/LivroController.java
+++ Downloads/bibliotec-api/bibliotec-api/src/main/java/com/bibliotec/bibliotec_api/controller/LivroController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,8 +40,14 @@ public class LivroController {
         return repository.findById(id).orElse(null);
     }
     
+     @PutMapping("/{id}")
+    public Livro alterar(@PathVariable Long id, @RequestBody Livro livro){
+        livro.setId(id);
+        return repository.save(livro);
+    }
+    
     @DeleteMapping("/{id}")
     public void excluir(@PathVariable Long id){
             repository.deleteById(id);
-}
+    }  
 }
