ff6cc83e7e5de364ad165d04885832ad2678a5e3
Patricia Lacerda <patriciagolfete@gmail.com>
2026-05-27 14:21:30 -0300

    CRUD completo Usuario

---
+++ Downloads/bibliotec-api/bibliotec-api/src/main/java/com/bibliotec/bibliotec_api/controller/UsuarioController.java
@@ -0,0 +1,48 @@
+
+package com.bibliotec.bibliotec_api.controller;
+
+import com.bibliotec.bibliotec_api.model.Usuario;
+import com.bibliotec.bibliotec_api.repository.UsuarioRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+ @Autowired
+    private UsuarioRepository repository;
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Usuario salvar(@RequestBody Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public Usuario buscarPorId(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario alterar(@PathVariable Long id, @RequestBody Usuario usuario) {
+        usuario.setId(id);
+        return repository.save(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }   
+}
