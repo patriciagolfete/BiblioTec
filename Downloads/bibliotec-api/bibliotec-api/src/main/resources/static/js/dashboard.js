49bde22ebf40e0b6a5029ccb33d2744c66e9c838
m4riavit0ria <2006mvms@gmail.com>
2026-06-16 14:10:35 -0300

    Separação HTML/CSS/JS - Login, Index e Dashboard

---
+++ Downloads/bibliotec-api/bibliotec-api/src/main/resources/static/js/dashboard.js
@@ -0,0 +1,16 @@
+function abrirLivros() {
+    window.location.href = "/livros.html";
+}
+
+function abrirUsuarios() {
+    window.location.href = "/usuarios.html";
+}
+
+function abrirEmprestimos() {
+    window.location.href = "/emprestimos.html";
+}
+
+function sair() {
+    window.location.href = "/login.html";
+}
+
